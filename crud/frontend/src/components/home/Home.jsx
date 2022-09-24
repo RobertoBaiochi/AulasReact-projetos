c66c99ce3aaeb89c94d0634c38cfc7dd43950301
Roberto Baiochi <107875368+RobertoBaiochi@users.noreply.github.com>
2022-09-23 22:02:48 -0300

    projeto crud

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => {
+  return (
+    <Main
+      icon="home"
+      title="Início"
+      subtitle="Segundo Projeto do Capítulo de React."
+    >
+      <div className="display-4">Bem Vindo!</div>
+      <hr />
+      <p className="mb-0">
+        Sistema para exemplificar a construção de um cadastro desenvolvido em
+        React!
+      </p>
+    </Main>
+  );
+};
